3b882c95992ba7311336578b04e36668873cd4a7
Samer Buna <samer.buna@gmail.com>
2017-08-31 09:51:14 -0700

    structure

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './store';
+import './styles/index.scss';
+
+import Game from './components/Game';
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const config = {
+  devtool: 'eval-source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader', // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+          {
+            loader: 'sass-loader', // compiles Sass to CSS
+          },
+        ],
+      },
+    ],
+  },
+};
+
+module.exports = config;
