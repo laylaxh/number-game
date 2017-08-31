415bed1ec448f6bd73d4cfed4cc79e3b0fead86b
Layla Habahbeh <laylahabahbeh@lc-laylahabahbeh.local>
2017-08-31 14:10:25 -0700

    Add initial game state

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,41 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { randomNumberGenerator } from '../store/util.js';
+import { connect } from 'react-redux';
+
+import { randomNumberGenerator } from '../store/util';
 import RandomNumbersPanel from './RandomNumbersPanel';
 
-// initial data:
-//  Target
-//  X numbers of randoms
-
 class Game extends React.Component {
-  
   static propTypes = {
-    numberCount: PropTypes.number.isRequired,  // define what it is in here
-  }
-
-  constructor(props){
+    numberCount: PropTypes.number.isRequired,
+    selectedNumbers: PropTypes.arrayOf(PropTypes.number).isRequired,
+  };
+  constructor(props) {
     super();
-    // we are importing randomNumberGenerator from store/util.js 
-    this.randomNumbers = Array.from({ 
+    this.randomNumbers = Array.from({
       length: props.numberCount,
     }).map(() => randomNumberGenerator());
-
     this.target = this.randomNumbers
       .slice(0, props.numberCount - 2)
       .reduce((acc, curr) => acc + curr);
   }
-
+  gameStatus = () => {
+    const sumSelected = this.props.selectedNumbers.reduce((acc, curr) => acc + this.randomNumbers[curr], 0);
+    if (sumSelected < this.target) {
+      return 'playing';
+    }
+    if (sumSelected === this.target) {
+      return 'won';
+    }
+    if (sumSelected > this.target) {
+      return 'lost';
+    }
+  };
   render() {
+    const gameStatus = this.gameStatus(); // Every time I render I need to compute the game status
     return (
-      <div>
+      <div id="game">
         <div id="target">{this.target}</div>
-        <RandomNumbersPanel randomNumbers={this.randomNumbers}/>
+        <RandomNumbersPanel randomNumbers={this.randomNumbers} />
+        {gameStatus}
       </div>
     );
   }
 }
 
-// props.NumberCount (going to be coming from App.js)
-
-
-export default Game;
+export default connect((state) => ({
+  selectedNumbers: state.selectedNumbers
+}))(Game);
